dc9f0ce239520120203e32151d307ba54003c0b7
Rezi <rsulaiman2@myseneca.ca>
2022-11-24 17:04:58 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import NotFound from "./components/NotFound";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route> 
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+
+
+
